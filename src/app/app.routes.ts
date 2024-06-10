f4b28e3cfc627c4d192007ac4fa99d68cc22ca05
Christina <mianaviatte@gmail.com>
2024-06-10 22:06:03 +0300

    Initialised homepage, article page and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from '../app/home/home.component'
+import { ArticleComponent } from '../app/article/article.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+      {
+        path: '',
+        children: [
+          {
+            path: '',
+            component: HomeComponent,
+          },
+          {
+            path: ':id',
+            component: ArticleComponent,
+          },
+        ],
+      },
+];

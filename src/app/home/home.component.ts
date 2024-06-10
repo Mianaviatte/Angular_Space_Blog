9b8913c6c4401d4311f498373d8302651ada3650
Christina <mianaviatte@gmail.com>
2024-06-10 22:32:24 +0300

    Initialised API SpaceFlightNews

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../service/api.service';
+import { HttpClientModule } from "@angular/common/http";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule],
+  providers: [ApiService],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  articles!: any[];
 
+  constructor(private apiService: ApiService) {
+    this.apiService.get().subscribe(result=>this.articles=result);
+  }
 }
---
+++ src/app/service/api.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from "rxjs";
+
+export interface ClientOptions {
+  body?: any;
+  headers?: | HttpHeaders | {
+    [header: string]: string | string[];
+  };
+  params?: | HttpParams | {
+    [param: string]: any | any[];
+  };
+  responseType?: any;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  getUrl = 'https://api.spaceflightnewsapi.net/v4/reports'
+
+  constructor(private readonly http: HttpClient,) { }
+
+  get(options: ClientOptions = { }): Observable<any> {
+    return this.http.get(this.getUrl, options);
+  }
+}

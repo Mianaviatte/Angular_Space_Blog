5041a05a5c7f227c3300862518fc7b2735dab5b9
Christina <mianaviatte@gmail.com>
2024-06-11 23:47:35 +0300

    Added  and implemented search bar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,26 +1,48 @@
-import { Component } from '@angular/core';
-import { ApiService } from '../service/api.service';
-import { HttpClientModule } from "@angular/common/http";
+import { Component, OnInit } from '@angular/core';
+import { ApiService, ClientOptions } from '../service/api.service';
 import {CommonModule} from '@angular/common';
 import {MatCardModule} from '@angular/material/card'; 
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 import {RouterLink}  from '@angular/router';
 import {MatDividerModule} from '@angular/material/divider';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HttpClientModule, CommonModule, MatCardModule, MatDividerModule, RouterLink],
+  imports: [CommonModule, MatCardModule, MatDividerModule, RouterLink, MatIconModule, MatButtonModule, FormsModule, MatInputModule, MatFormFieldModule],
   providers: [ApiService],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  value = '';
+
   articles!: any;
 
   constructor(private apiService: ApiService) {
-    this.apiService.get().subscribe(result=>{
-      this.articles=result;
-      console.log('result is ', result);
-    });
+  }
+
+ ngOnInit(): void {
+  this.getItems();
+ }
+
+ getItems(search = '') :void {
+   const options: ClientOptions = {
+     ...search && {
+      params: {
+        search,
+      }
+     }
+   }
+
+   this.apiService.get(options).subscribe( result=>{
+     this.articles = result;
+     console.log('result is ', result, options);
+   });
   }
 }
